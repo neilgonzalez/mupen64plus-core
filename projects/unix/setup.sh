641e8835c1ef31edb73ae795785e52d4613e5917
Richard Goedeken <Richard@fascinationsoftware.com>
2019-02-10 10:06:05 -0800

    update version and release info for v2.5.9 BETA release
    quick setup

---
+++ projects/unix/setup.sh
@@ -0,0 +1,3 @@
+sudo apt install libpng-dev pkg-config zlibc zlib1g zlib1g-dev \
+libsdl2-dev libglade2-dev nasm libboost-filesystem-dev \
+python-pyqt5 pyqt5-dev-tools python-pyqt5.qtopengl libsdl2-dev python-sdl2
